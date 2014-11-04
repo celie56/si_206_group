6bc0d37425375140c925af6f77a36c4f0d648f74
martinalu <elisalu@umich.edu>
2014-11-04 18:17:11 -0500

    validation done

--- validation.js
+++ validation.js
@@ -1,12 +1,15 @@
 
 
-function check(input) {
+function checkEmail(input) {
   if (input.value != document.getElementById('email_addr').value) {
     input.setCustomValidity('The two email addresses must match.');
   } else {
     // input is valid -- reset the error message
     input.setCustomValidity('');
   }
+
+  else if(input.value == null) {
+    input.setCustomValidity('Input your email');
 }
 
 
@@ -19,4 +22,17 @@ function checkAge(input) {
     // input is valid -- reset the error message
     input.setCustomValidity('');
   }
+}
+
+
+
+function checkName(input) {
+  if (input.value == null) {
+    input.setCustomValidity('Input your age.');
+  } 
+
+  // else {
+  //   // input is valid -- reset the error message
+  //   input.setCustomValidity('');
+  // }
 }
