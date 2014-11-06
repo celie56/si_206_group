f01b5b06647d939feb9bb4f62df5b047a47b8161
Chris <celie56@gmail.com>
2014-11-06 10:44:43 -0500

    Updated js syntax and added age checking

--- validation.js
+++ validation.js
@@ -1,38 +1,32 @@
 
 
-function checkEmail(input) {
-  if (input.value != document.getElementById('email_addr').value) {
-    input.setCustomValidity('The two email addresses must match.');
-  } else {
-    // input is valid -- reset the error message
-    input.setCustomValidity('');
-  }
-
-  else if(input.value == null) {
-    input.setCustomValidity('Input your email');
+var checkEmail = function(input) {
+	if (input.value != document.getElementById('email_addr').value) {
+		input.setCustomValidity('The two email addresses must match.');
+	}
+	else if(input.value == null) {
+		input.setCustomValidity('Input your email');
+	}
+	else {
+		// input is valid -- reset the error message
+		input.setCustomValidity('');
+	}
 }
 
-
-
-
-function checkAge(input) {
-  if (input.value == null) {
-    input.setCustomValidity('Input your age.');
-  } else {
-    // input is valid -- reset the error message
-    input.setCustomValidity('');
-  }
+var checkAge = function(input) {
+	if (input.value == null) {
+		input.setCustomValidity('Input your age.');
+	}
+	else if (input.value < 0) {
+		input.setCustomValidity('Please select an age above 0');
+	}
+	else {
+		input.setCustomValidity('');
+	}
 }
 
-
-
-function checkName(input) {
-  if (input.value == null) {
-    input.setCustomValidity('Input your age.');
-  } 
-
-  // else {
-  //   // input is valid -- reset the error message
-  //   input.setCustomValidity('');
-  // }
+var checkName = function(input) {
+	if (input.value == '') {
+		input.setCustomValidity('Please put in your name.');
+	} 
 }
