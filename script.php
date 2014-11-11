e1b51b72e3b5a0d08d61ad4cfb2e914bcccfc6e6
Chris <celie56@gmail.com>
2014-11-11 10:26:25 -0500

    Updated php tags

--- script.php
+++ script.php
@@ -1,8 +1,5 @@
 <?php
-$myfile = fopen("test.txt", "w") or die("Unable to open file!");
-$txt = "John Doe\n";
-fwrite($myfile, $txt);
-$txt = "Jane Doe\n";
-fwrite($myfile, $txt);
-fclose($myfile);
+foreach($_GET as $k => $v){
+	echo "Key=",$k," Val=", $v, "\n";
+}
 ?>
