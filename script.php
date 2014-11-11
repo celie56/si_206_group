4290d72c16ced99baad89337db79a02ac3deb9a2
Chris <celie56@gmail.com>
2014-11-11 16:27:47 -0500

    script.php now writes to file userinput.txt

--- script.php
+++ script.php
@@ -1,5 +1,11 @@
 <?php
+$fp = fopen("userinput.txt", "a");
+fwrite($fp, $message);
 foreach($_GET as $k => $v){
 	print "$k: $v<br>";
+	fwrite($fp, "$k: $v, ");
+	$message += "$k: $v,";
 }
+fwrite($fp, "END\n");
+fclose($fp);
 ?>
