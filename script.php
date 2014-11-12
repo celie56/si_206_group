12aa42e45dd4805bc7e42de8e7f919145d7c3003
martinalu <elisalu@umich.edu>
2014-11-11 19:15:05 -0500

    got the email working

--- script.php
+++ script.php
@@ -8,4 +8,22 @@ foreach($_GET as $k => $v){
 }
 fwrite($fp, "END\n");
 fclose($fp);
+
+//registration email
+  $message="";
+  foreach($_POST as $n => $val) {
+	
+   $message .="$n : $val\n";
+	
+  }
+
+	$to = $_POST["email"];
+
+	$subject = 'Thank you for registering';
+	$headers = 'From: webmaster@example.com' . "\r\n" .
+		    'Reply-To: webmaster@example.com' . "\r\n" .
+			    'X-Mailer: PHP/' . phpversion();
+
+	mail($to, $subject, $message, $headers);
+	echo "Email sent";
 ?>
