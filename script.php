3feccf13f905834ab508a44c04346824ce229004
Chris <celie56@gmail.com>
2014-11-11 23:49:00 -0500

    Email me box and php

--- script.php
+++ script.php
@@ -1,18 +1,16 @@
 <?php
-$fp = fopen("userinput.txt", "a");
-fwrite($fp, $message);
-foreach($_POST as $k => $v){
-	print "$k: $v<br>";
-	fwrite($fp, "$k: $v, ");
-	$message += "$k: $v,";
-}
-fwrite($fp, "END\n");
-fclose($fp);
+	$fp = fopen("userinput.txt", "a");
+	fwrite($fp, $message);
+	foreach($_POST as $k => $v){
+		print "$k: $v<br>";
+		fwrite($fp, "$k: $v, ");
+		$message += "$k: $v,";
+	}
+	fwrite($fp, "END\n");
+	fclose($fp);
 
-//registration email
-}
-
-	if (false){
+	//registration email
+	if (isset($_POST['emailme']) && $_POST['emailme'] == "yes"){
 		$to = $_POST["email"];
 		$subject =	'Thank you for registering';
 		$headers =	'From: webmaster@example.com' . "\r\n" .
