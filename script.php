8f590a0337557183570a47f0ac8e86adf059f764
Chris <celie56@gmail.com>
2014-11-11 22:15:59 -0500

    Added email option checkbox

--- script.php
+++ script.php
@@ -1,7 +1,7 @@
 <?php
 $fp = fopen("userinput.txt", "a");
 fwrite($fp, $message);
-foreach($_GET as $k => $v){
+foreach($_POST as $k => $v){
 	print "$k: $v<br>";
 	fwrite($fp, "$k: $v, ");
 	$message += "$k: $v,";
@@ -10,20 +10,16 @@ fwrite($fp, "END\n");
 fclose($fp);
 
 //registration email
-  $message="";
-  foreach($_POST as $n => $val) {
-	
-   $message .="$n : $val\n";
-	
-  }
+}
 
-	$to = $_POST["email"];
+	if (false){
+		$to = $_POST["email"];
+		$subject =	'Thank you for registering';
+		$headers =	'From: webmaster@example.com' . "\r\n" .
+					'Reply-To: webmaster@example.com' . "\r\n" .
+					'X-Mailer: PHP/' . phpversion();
 
-	$subject = 'Thank you for registering';
-	$headers = 'From: webmaster@example.com' . "\r\n" .
-		    'Reply-To: webmaster@example.com' . "\r\n" .
-			    'X-Mailer: PHP/' . phpversion();
-
-	mail($to, $subject, $message, $headers);
-	echo "Email sent";
+		mail($to, $subject, $message, $headers);
+		print "Email sent";
+	}
 ?>
