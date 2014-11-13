79f5bfad9bf9fa17ff4a43c00582a1396fe449ed
Chris <celie56@gmail.com>
2014-11-13 14:13:33 -0500

    Confirmation message update

--- script.php
+++ script.php
@@ -1,25 +1,23 @@
 <?php
 	
 	$fp = fopen("userinput.txt", "a");
-	$message="";
-	fwrite($fp, $message);
+	$message = "Email confirmation:\n";
 	foreach($_POST as $key => $value){
 		if($key == "interests")
 		{
-			print "$key:";
 			foreach($_POST['interests'] as $interest)
 			{
 				$interest = implode(', ', $_POST['interests']);
 			}
-			print(" $interest <br>");
-			fwrite($fp, "$key: $interest ");
-			$message+= "$key: $interest; ";
+			print("$key: $interest <br>");
+			fwrite($fp, "$key: $interest; ");
+			$message .= "$key: $interest\n";
 		}
 		else
 		{
 			print "$key: $value<br>";
-			fwrite($fp, "$key: $value ");
-			$message += "$key: $value; ";
+			fwrite($fp, "$key: $value; ");
+			$message .= "$key: $value\n";
 		}	
 			
 	}
@@ -30,9 +28,9 @@
 	//registration email
 	if (isset($_POST['emailme']) && $_POST['emailme'] == "yes"){
 		$to = $_POST["email"];
-		$subject =	'Thank you for registering';
-		$headers =	'From: webmaster@example.com' . "\r\n" .
-					'Reply-To: webmaster@example.com' . "\r\n" .
+		$subject =	'Thank you for registering with si 206';
+		$headers =	'From: si_206@example.com' . "\r\n" .
+					'Reply-To: si_206@example.com' . "\r\n" .
 					'X-Mailer: PHP/' . phpversion();
 
 		mail($to, $subject, $message, $headers);
