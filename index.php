780d951c1864426e8fb9cb28f8e095dc481058b3
celie56 <celie56@gmail.com>
2014-11-23 23:57:39 -0500

    restructuring for php templating

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<title>Battle Royal</title>
-		<meta charset="utf-8"> <!-- yup, we got charmander -->
-		<meta name="viewport" content="width=device-width,initial-scale=1">		<!-- Got that viewport all right -->
-		<script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"></script>		<!-- JQuery Hosted Externally -->
-		<link href="bootstrap.min.css" rel="stylesheet">		<!-- Bootstrappin Hosted Externally -->
-		<link id="theme" rel="stylesheet" href="">		<!-- Theme to be changed -->
-	</head>
 	<?php
         // Just in case we want to change these easily
         $header = "header.php";
         $landing = "home.html";
         $footer = "footer.php";
+        $styles = "styles.html";
+        $scripts = "scripts.html";
 	?>
+	<head>
+		<title>Battle Royal</title>
+		<meta charset="utf-8"> <!-- yup, we got charmander -->
+		<meta name="viewport" content="width=device-width,initial-scale=1">		<!-- Got that viewport all right -->
+        <?php include "$styles"; ?>
+	</head>
 	<body>
 		<?php include "$header"; ?>
 		<div id="content" class="">
@@ -21,7 +21,7 @@
 			<?php include "$landing"; ?>
 		</div>
 		<?php include "$footer"; ?>
-		<script type="text/javascript" src="login.js"></script>		<!-- Our Javascript, yet to be set -->
+		<?php include "$scripts"; ?>
 	</body>
 </html>
 
