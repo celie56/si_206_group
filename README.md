f151b448730ab3b803d3a475f26fffba7d898bd1
celie56 <celie56@gmail.com>
2014-11-26 18:54:09 -0500

    added comments

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ si_206_group
 
 To Do:
 Make the rest of the website
+Also, check out the todo.txt for more info
 
 Website and form designed by:
 Martina Lugo, Chris Elie, Yoav Helfman
--- header.php
+++ header.php
@@ -1,5 +1,11 @@
 <nav>
 	<ul>
+        <!-- 
+            Quick explanation:
+            The javascript will grab the text between the li tags,
+            append .html to it, and then put in in the content div
+            for a long explanation check out utilities.js
+        -->
 		<li class="link">Home</li>
 		<li class="link">About</li>
 		<li class="link">Extra</li>
