037eb0a6a4b63a5397a4cdb0fb3dbc289f1678d9
Chris <celie56@gmail.com>
2014-11-28 04:04:42 +0000

    Added some bootstrap classes

--- header.php
+++ header.php
@@ -1,15 +1,15 @@
 <nav>
-	<ul>
+	<ul class="nav navbar">
         <!-- 
             Quick explanation:
             The javascript will grab the text between the li tags,
             append .html to it, and then put in in the content div
             for a long explanation check out utilities.js
         -->
-		<li class="link">Home</li>
-		<li class="link">About</li>
-		<li class="link">Extra</li>
-		<li class="link">4th</li>
-		<li class="link">5th</li>
+		<li class="link nav-pill">Home</li>
+		<li class="link nav-pill">About</li>
+		<li class="link nav-pill">Extra</li>
+		<li class="link nav-pill">4th</li>
+		<li class="link nav-pill">5th</li>
 	</ul>
 </nav>
