6fe46e554c059094ff6951df7b03070edd674173
martinalu <elisalu@umich.edu>
2014-12-02 14:15:52 -0500

    added a theme. Please don't kill me

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
 
-<footer>
-	<div>
-		Such footer 	
-	</div>
-	<?php
-		$filename = basename($_SERVER['SCRIPT_NAME']);
-		echo "$filename was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
-	?>
-</footer>
+   
+<div class="row">
+    <div class="col-lg-12">
+        <?php
+			$filename = basename($_SERVER['SCRIPT_NAME']);
+			echo "$filename was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
+			?>
+    </div>
+</div>
+<!-- /.row -->
+        
--- header.php
+++ header.php
@@ -1,15 +1,30 @@
-<nav>
-	<ul class="nav navbar">
-        <!-- 
-            Quick explanation:
-            The javascript will grab the text between the li tags,
-            append .html to it, and then put in in the content div
-            for a long explanation check out utilities.js
-        -->
-		<li class="link nav-pill">Home</li>
-		<li class="link nav-pill">About</li>
-		<li class="link nav-pill">Extra</li>
-		<li class="link nav-pill">4th</li>
-		<li class="link nav-pill">5th</li>
-	</ul>
-</nav>
+ <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+          
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+		                    <!-- 
+		            Quick explanation:
+		            The javascript will grab the text between the li tags,
+		            append .html to it, and then put in in the content div
+		            for a long explanation check out utilities.js
+		        -->
+					<li class="link nav-pill">Home</li> 
+					<li class="link nav-pill">About</li>
+					<li class="link nav-pill">Extra</li>
+					<li class="link nav-pill">4th</li>
+					<li class="link nav-pill">5th</li>
+		        </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+</div>
+<!-- /.container -->
