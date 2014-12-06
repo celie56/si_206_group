a5410eda5aacfb0f147f05326bc1025fb6b944df
martinalu <elisalu@umich.edu>
2014-12-06 15:51:49 -0500

    centered footer

--- footer.php
+++ footer.php
@@ -1,7 +1,6 @@
-
-   
+  
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-12 text-center">
         <?php
 			$filename = basename($_SERVER['SCRIPT_NAME']);
 			echo "$filename was last modified: " . date ("F d Y H:i:s.", filemtime($filename));
@@ -9,4 +8,6 @@
     </div>
 </div>
 <!-- /.row -->
+        
+
         
