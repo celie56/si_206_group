962810104420a04d8be2b785008b5f3b6b2d43b7
Chris <celie56@gmail.com>
2014-12-08 17:28:05 -0500

    fixed header styling

--- header.php
+++ header.php
@@ -10,11 +10,11 @@
                 append .html to it, and then put in in the content div
                 for a long explanation check out utilities.js
             -->
-            <li class=" nav-pill navigation"><a href="javascript:;">Home</a></li>
-            <li class=" nav-pill navigation"><a class="link" href="#" onclick="clicked(this);">About</a></li>
-            <li class="link nav-pill navigation"><a class="link" href="#" onclick="clicked(this);">Extra</a></li>
-            <li class="link nav-pill navigation"><a class="link" href="#" onclick="clicked(this);">Form</a></li>
-            <li class="link nav-pill navigation"><a class="link" href="#" onclick="clicked(this);">Gallery</a></li>
+            <li class="nav-pill"><a class="link" href="#" onclick="clicked(this);">Home</a></li>
+            <li class="nav-pill"><a class="link" href="#" onclick="clicked(this);">About</a></li>
+            <li class="nav-pill"><a class="link" href="#" onclick="clicked(this);">Extra</a></li>
+            <li class="nav-pill"><a class="link" href="#" onclick="clicked(this);">Form</a></li>
+            <li class="nav-pill"><a class="link" href="#" onclick="clicked(this);">Gallery</a></li>
 
             <li class="link nav-pill"><img id="user_avatar" class="header_img" alt="User Avatar" src="blank.PNG"></li>
         </ul>
