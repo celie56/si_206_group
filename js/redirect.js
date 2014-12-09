be596900bf1af2b1cc5510fa8b3be84e840113cd
Chris <celie56@gmail.com>
2014-12-08 23:03:04 -0500

    form stuff

---
+++ js/redirect.js
@@ -0,0 +1,3 @@
+
+alert("You will now be redirected to the homepage");
+window.location.href = "http://elietesting.cloudapp.net/si/group";
--- php/submit.php
+++ php/submit.php
@@ -1,22 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
 	// this script will run once the user has submitted data to our form
 	$fp = fopen("userinput.txt", "a");		// file to write all data to
 	$message = "Email confirmation:\n";		// This will be the message to email users
+    $content = "";
+    $character = "";
+    $color = "";
+    $background_color = "";
 	foreach($_POST as $key => $value){
 		if($key == "interests"){			// Parse interest list
 			foreach($_POST['interests'] as $interest){
 				$interest = implode(', ', $_POST['interests']);
 			}
-			print("$key: $interest <br>");
+			$content .= "$key: $interest <br>";
 			fwrite($fp, "$key: $interest; ");
 			$message .= "$key: $interest\n";
 		}
 		else{								// Parse all other inputs
-			print "$key: $value<br>";
+			$content .= "$key: $value<br>";
 			fwrite($fp, "$key: $value; ");
 			$message .= "$key: $value\n";
 		}	
+        if ($key == "character")
+            $character = $value;
+        if ($key == "color")
+            $color = $value;
+        if ($key == "background_color")
+            $background_color = $value;
 	}
+
+    $image = $character . "_" . $color . ".png";
 
 	fwrite($fp, "END\n");					// Add final message and close file
 	fclose($fp);
@@ -34,3 +48,14 @@
 	}
 	// That's all folks
 ?>
+	<head>
+	</head>
+    <body>
+        <?php print $content; ?>
+        <div>
+			<img alt="User Avater" src="../pics/<?php print $image; ?>">
+        </div>
+    </body>
+		<script type="text/javascript" src="../js/redirect.js"></script>
+        
+</html>
